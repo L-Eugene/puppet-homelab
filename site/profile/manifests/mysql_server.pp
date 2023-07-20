a837fc845b9c29df23a62e78c4e670158963b9af
Eugene Lapeko <eugene@lapeko.info>
2023-07-20 23:52:15 +0300

    MySQL Server should listen all the network

--- site/profile/manifests/mysql_server.pp
+++ site/profile/manifests/mysql_server.pp
@@ -1,5 +1,11 @@
 class profile::mysql_server {
   class { 'mysql::server':
-    restart => true,
+    restart                 => true,
+    reload_on_config_change => true,
+    override_options        => {
+      'mysqld' => {
+        'bind-address' => '0.0.0.0'
+      }
+    }
   }
 }
