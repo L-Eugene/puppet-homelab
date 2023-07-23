acb666f2d7ee0eda99f3c7585f5e071ca469a59b
Eugene Lapeko <eugene@lapeko.info>
2023-07-23 16:36:41 +0300

    Uze bzcat for DB restore

--- site/profile/manifests/gnucash_database.pp
+++ site/profile/manifests/gnucash_database.pp
@@ -6,8 +6,10 @@ class profile::gnucash_database {
     password => $gnucash_users[0]['password'],
     host     => '%',
     grant    => $gnucash_users[0]['grant'],
+
     # TODO: restore data from backup unless database exists
-    sql => '/backup/epam-nb/gnucash-latest.sql.bz2',
-    enforce_sql => false,
+    sql            => '/backup/epam-nb/gnucash-latest.sql.bz2',
+    import_cat_cmd => 'bzcat',
+    enforce_sql    => false,
   }
 }
