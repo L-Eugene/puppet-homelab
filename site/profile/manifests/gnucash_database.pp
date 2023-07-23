e4536dacc65b0f74be4400aa6d6cb92b8894db13
Eugene Lapeko <eugene@lapeko.info>
2023-07-23 17:29:20 +0300

    Backup gnucash DB on schedule

--- site/profile/manifests/gnucash_database.pp
+++ site/profile/manifests/gnucash_database.pp
@@ -12,4 +12,22 @@ class profile::gnucash_database {
     import_cat_cmd => 'bzcat',
     enforce_sql    => false,
   }
+
+  file { '/root/gnucash-backup.sh':
+    content => "#!/bin/bash
+      date=$(date +"%Y-%m-%d")
+      mysqldump gnucash --no-tablespaces | bzip2 /tmp/gnucash-$date.sql.bz2
+      cp /tmp/gnucash-$date.sql.bz2 /backup/homelab/gnucash-$date.sql.bz2
+      mv /tmp/gnucash-$date.sql.bz2 /backup/homelab/gnucash-latest.sql.bz2
+      ",
+    mode    => '0755',
+    owner   => 'root',
+  }
+
+  cron { 'backup-gnucash-db':
+    command => '/root/gnucash-backup.sh',
+    user    => 'root',
+    hour    => 3,
+    minute  => 0,
+  }
 }
