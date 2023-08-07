5f85f647241f2d55a75d2c2606c2a9fc08fa24fb
Eugene Lapeko <eugene@lapeko.info>
2023-08-07 22:09:32 +0300

    Trying to fix template location

--- site/profile/manifests/vkinformerbot.pp
+++ site/profile/manifests/vkinformerbot.pp
@@ -6,7 +6,7 @@ class profile::vkinformerbot {
   file { '/opt/vkinformer/docker-compose.yml':
     ensure => file,
     content => epp(
-      'docker-compose.yml.epp', 
+      'telegram_bot/docker-compose.yml.epp', 
       { 
         'bot_image' => 'L-Eugene/vkinformerbot',  
         'bot_volumes' => ['vk_informer_bot.rb.yml:/usr/src/app/vk_informer_bot.rb.yml']
@@ -17,7 +17,7 @@ class profile::vkinformerbot {
   file { '/opt/vkinformer/vk_informer_bot.rb.yml':
     ensure => file,
     content => epp(
-      'botconfig.yml.epp',
+      'telegram_bot/botconfig.yml.epp',
       { 'debug' => false }
     )
   }
