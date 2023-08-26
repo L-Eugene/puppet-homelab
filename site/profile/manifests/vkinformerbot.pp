823f7749bc3833986958ab8330db6a1ac2b119db
Eugene Lapeko <eugene@lapeko.info>
2023-08-26 23:11:24 +0300

    Configure bot DB

--- site/profile/manifests/vkinformerbot.pp
+++ site/profile/manifests/vkinformerbot.pp
@@ -14,11 +14,20 @@ class profile::vkinformerbot {
     )
   }
 
+  $vkinformer_user = lookup('vkinformer_user')
+  $vkinformer_base = lookup('vk_informer_db')
+
   file { '/opt/vkinformer/vk_informer_bot.rb.yml':
     ensure => file,
     content => epp(
       'telegram_bot/botconfig.yml.epp',
-      { 'debug' => false }
+      { 
+        'debug' => false,
+        'db_host' => $vkinformer_base['db_host'],
+        'db_name' => $vkinformer_base['db_name'],
+        'db_user' => $vkinformer_user['username'],
+        'db_pass' => $vkinformer_user['password'],
+      }
     )
   }
 }
