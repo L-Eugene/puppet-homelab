96f1dd1c62c1cbc0ef57388b7667ee93b25b169d
Eugene Lapeko <eugene@lapeko.info>
2023-08-27 16:39:30 +0300

    Fix bot compose configuration

--- site/profile/manifests/vkinformerbot.pp
+++ site/profile/manifests/vkinformerbot.pp
@@ -8,8 +8,8 @@ class profile::vkinformerbot {
     content => epp(
       'telegram_bot/docker-compose.yml.epp', 
       { 
-        'bot_image' => 'L-Eugene/vkinformerbot',  
-        'bot_volumes' => ['vk_informer_bot.rb.yml:/usr/src/app/vk_informer_bot.rb.yml']
+        'bot_image' => 'elapeko/vkinformerbot',  
+        'bot_volumes' => ['./vk_informer_bot.rb.yml:/usr/src/app/vk_informer_bot.rb.yml']
       }
     )
   }
