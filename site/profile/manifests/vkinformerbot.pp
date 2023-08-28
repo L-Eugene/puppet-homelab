03baf8eadc8c8470cc3719481f8916f26d537358
Eugene Lapeko <eugene@lapeko.info>
2023-08-28 22:25:53 +0300

    Define class for telegram_bot configuration

--- site/profile/manifests/vkinformerbot.pp
+++ site/profile/manifests/vkinformerbot.pp
@@ -1,54 +1,16 @@
 class profile::vkinformerbot {
-  file { '/opt/vkinformer':
-    ensure => directory,
-  }
-
-  file { '/opt/vkinformer/docker-compose.yml':
-    ensure => file,
-    content => epp(
-      'telegram_bot/docker-compose.yml.epp', 
-      { 
-        'bot_image' => 'elapeko/vkinformerbot',  
-        'bot_volumes' => ['./vk_informer_bot.rb.yml:/usr/src/app/vk_informer_bot.rb.yml']
-      }
-    )
-  }
-
   $vkinformer_user = lookup('vkinformer_user')
   $vkinformer = lookup('vk_informer')
 
-  file { '/opt/vkinformer/vk_informer_bot.rb.yml':
-    ensure => file,
-    content => epp(
-      'telegram_bot/botconfig.yml.epp',
-      { 
-        'debug' => false,
-        'db_host'  => $vkinformer['db']['db_host'],
-        'db_name'  => $vkinformer['db']['db_name'],
-        'db_user'  => $vkinformer_user['username'],
-        'db_pass'  => $vkinformer_user['password'],
-        'tg_token' => $vkinformer['tg']['token'],
-      }
-    )
-  }
-
-  systemd::manage_unit { 'vkinformer.service':
-    unit_entry    => {
-      'Description' => 'VK Informer Telegram bot',
-      'PartOf'      => 'docker.service',
-      'After'       => 'docker.service',
-    },
-    service_entry => {
-      'Type'              => 'oneshot',
-      'RemainAfterExit'   => true,
-      'WorkingDirectory'  => '/opt/vkinformer/',
-      'ExecStart'         => 'docker-compose up -d --remove-orphans',
-      'ExecStop'          => 'docker-compose down',
-    },
-    install_entry => {
-      'WantedBy' => 'multi-user.target',
-    },
-    enable        => true,
-    active        => true,
+  telegram_bot { 'vkinformer':
+    bot_config_file => 'vk_informer_bot.rb.yml',
+    bot_image       => 'elapeko/vkinformerbot',
+    bot_token       => $vkinformer['tg']['token'],
+    bot_database    => {
+      'db_host'  => $vkinformer['db']['db_host'],
+      'db_name'  => $vkinformer['db']['db_name'],
+      'db_user'  => $vkinformer_user['username'],
+      'db_pass'  => $vkinformer_user['password'],
+    }
   }
 }
---
+++ site/telegram_bot/manifests/init.pp
@@ -0,0 +1,51 @@
+class telegram_bot (
+  String $bot_config_file = "${$name}.rb.yml",
+  String $bot_image,
+  String $bot_app_path = '/usr/src/app',
+  Array  $bot_volumes = [],
+  String $bot_debug = false,
+  String $bot_token = 'UNDEFINED',
+  Hash   $bot_database = {}
+) {
+  file { "/opt/${$name}":
+    ensure => directory,
+  }
+
+  file { "/opt/${$name}/docker-compose.yml":
+    ensure => file,
+    content => epp(
+      'telegram_bot/docker-compose.yml.epp', 
+      { 
+        'bot_image'   => $bot_image,  
+        'bot_volumes' => ["./${$bot_config_file}:${$bot_app_path}/${$bot_config_file}"] + $bot_volumes,
+      }
+    )
+  }
+
+  $config_parameters = { 'db_name' => '', 'db_pass' => '' } + { 'debug' => $bot_debug } + $bot_database
+
+  file { "/opt/${$name}/${$bot_config_file}":
+    ensure => file,
+    content => epp('telegram_bot/botconfig.yml.epp', $config_parameters)
+  }
+
+  systemd::manage_unit { "${$name}.service":
+    unit_entry    => {
+      'Description' => "${$name} Telegram bot",
+      'PartOf'      => 'docker.service',
+      'After'       => 'docker.service',
+    },
+    service_entry => {
+      'Type'              => 'oneshot',
+      'RemainAfterExit'   => true,
+      'WorkingDirectory'  => "/opt/${$name}/",
+      'ExecStart'         => 'docker-compose up -d --remove-orphans',
+      'ExecStop'          => 'docker-compose down',
+    },
+    install_entry => {
+      'WantedBy' => 'multi-user.target',
+    },
+    enable        => true,
+    active        => true,
+  }
+}
