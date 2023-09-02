fd4080efb0ad0d72a4b0c3a2ded990d9b3de6c49
Eugene Lapeko <eugene@lapeko.info>
2023-09-02 19:14:13 +0300

    Support several GNUCash users

--- site/profile/manifests/gnucash_database.pp
+++ site/profile/manifests/gnucash_database.pp
@@ -2,15 +2,18 @@ class profile::gnucash_database {
   $gnucash_users = lookup('gnucash_users')
 
   mysql::db { 'gnucash':
-    user     => $gnucash_users[0]['username'],
-    password => $gnucash_users[0]['password'],
-    host     => '%',
-    grant    => $gnucash_users[0]['grant'],
-
-    # TODO: restore data from backup unless database exists
     sql            => ['/backup/homelab/gnucash-latest.sql.bz2'],
     import_cat_cmd => 'bzcat',
     enforce_sql    => false,
+  }
+
+  $gnucash_users.each |$user| {
+    mysql::db { 'gnucash':
+      user     => $user['username'],
+      password => $user['password'],
+      host     => '%',
+      grant    => $user['grant'],
+    }
   }
 
   cron { 'backup-gnucash-db':
