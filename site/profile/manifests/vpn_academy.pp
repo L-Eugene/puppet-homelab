8ac91feceb88a96835c8dd19d9e61fb641bda357
Eugene Lapeko <eugene@lapeko.info>
2023-11-27 22:33:15 +0200

    Setup academy VPN server

---
+++ site/profile/manifests/vpn_academy.pp
@@ -0,0 +1,87 @@
+class profile::vpn_academy {
+  $file_content = lookup('vpn_academy')
+
+  file { '/etc/openvpn/':
+    ensure  => directory,
+    mode    => "0755",
+    owner   => root,
+    group   => root
+  }
+
+  file { '/etc/openvpn/academy/':
+    ensure  => directory,
+    mode    => "0755",
+    owner   => root,
+    group   => root
+  }
+
+  file { '/etc/openvpn/academy/ca.crt':
+    ensure  => file,
+    content => $file_content['ca'],
+    mode    => "0644",
+    owner   => root,
+    group   => root
+  }
+
+  file { '/etc/openvpn/academy/dh.pem':
+    ensure  => file,
+    content => $file_content['dh'],
+    mode    => "0644",
+    owner   => root,
+    group   => root
+  }
+
+  file { '/etc/openvpn/academy/vps.crt':
+    ensure  => file,
+    content => $file_content['cert'],
+    mode    => "0644",
+    owner   => root,
+    group   => root
+  }
+
+  file { '/etc/openvpn/academy/vps.key':
+    ensure  => file,
+    content => $file_content['key'],
+    mode    => "0600",
+    owner   => root,
+    group   => root
+  }
+
+  class {'openvpn':
+    server_name => 'academy',
+    server_config => {
+        'port' => 1195,
+        'proto' => 'tcp',
+        'dev' => 'tun',
+        'ca' => '/etc/openvpn/academy/ca.crt',
+        'cert' => '/etc/openvpn/academy/vps.crt',
+        'key' => '/etc/openvpn/academy/vps.key',
+        'dh' => '/etc/openvpn/academy/dh2048.pem',
+        'topology' => 'subnet',
+        'server' => '10.202.0.0 255.255.255.0',
+        'ifconfig-pool-persist' => '/etc/openvpn/academy/ipp.txt',
+        'push' => [
+            '"route 172.20.6.0 255.255.255.0"',
+            '"route 172.20.7.0 255.255.255.0"',
+            '"route 192.168.20.0 255.255.255.0"',
+        ],
+        'client-config-dir' => '/etc/openvpn/academy/ccd',
+        'client-to-client' => undef,
+        'keepalive' => '10 120',
+        'cipher' => 'AES-256-CBC',
+        'auth' => 'SHA1',
+        'max-clients' => 100,
+        'persist-key' => undef,
+        'persist-tun' => undef
+    },
+    ccd_options => {
+      'itacademy-mikrotik' => { 
+        'iroute' => [
+          '172.20.6.0 255.255.255.0',
+          '172.20.7.0 255.255.255.0',
+          '192.168.20.0 255.255.255.0',
+        ]
+      }
+    }
+  }
+}
--- site/role/manifests/openvpn_server.pp
+++ site/role/manifests/openvpn_server.pp
@@ -1,3 +1,4 @@
 class role::openvpn_server{
   include profile::vpn_private
+  include profile::vpn_academy
 }
