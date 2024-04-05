e2f3e3bf49ec968609ae283f7ca6653cdaba73ed
Eugene Lapeko <eugene@lapeko.info>
2024-04-05 20:06:11 +0000

    Dev container local test improvements

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -4,3 +4,8 @@ for file in manifests site Puppetfile
 do
     ln -s "/workspaces/puppet-homelab/${file}" "/local-test/${file}"
 done
+
+ln -s "/workspaces/puppet-homelab/test-data/common.yaml" "/local-test/hieradata/external.yaml"
+
+cd /local-test/
+r10k puppetfile install
--- .devcontainer/local-test/pa.sh
+++ .devcontainer/local-test/pa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-puppet apply --modulepath site:\$basemodulepath --hiera_config /local-test/hiera.yaml $@
+puppet apply --modulepath site:modules:\$basemodulepath --hiera_config /local-test/hiera.yaml $@
