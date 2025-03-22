8074b4a64cac127a635720946b9126547f87c105
Eugene Lapeko <eugene@lapeko.info>
2025-03-22 19:29:12 +0000

    Create role

--- manifests/site.pp
+++ manifests/site.pp
@@ -20,3 +20,7 @@ node 'openvpn.lan' {
 node 'minecraft.lan' {
   include role::minecraft_server
 }
+
+node 'eugene-VirtualBox' {
+  include role::laptop
+}
---
+++ site/role/manifests/laptop.pp
@@ -0,0 +1,4 @@
+# This class configures the laptop role by including necessary profiles.
+class role::laptop {
+  include profile::lab_timezone
+}
