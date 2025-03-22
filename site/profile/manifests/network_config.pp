90997fb04fb531ee2249683fa669ccc8d43c08f9
Eugene Lapeko <eugene@lapeko.info>
2025-03-22 20:51:07 +0000

    Remove unneeded code

--- site/profile/manifests/network_config.pp
+++ site/profile/manifests/network_config.pp
@@ -2,32 +2,4 @@
 class profile::network_config {
   $configs = lookup('network_config')
   create_resources('networkmanager::connection', $configs)
-/*
-  networkmanager::connection { 'ass-teriks':
-    content => {
-      connection    => {
-        'id'          => 'ass-teriks',
-        'type'        => 'wifi',
-        'permissions' => ''
-      },
-      wifi          => {
-        'ssid' => 'ass-teriks',
-        'mode' => 'infrastructure'
-      },
-      wifi-security => {
-        'key-mgmt' => 'wpa-psk',
-        'psk'      => 'password'
-      },
-      ipv4          => {
-        'dns-search' => '',
-        'method'     => 'auto'
-      },
-      ipv6          => {
-        'dns-search'    => '',
-        'method'        => 'auto',
-        'addr-gen-mode' => 'stable-privacy'
-      }
-    },
-  }
-  */
 }
