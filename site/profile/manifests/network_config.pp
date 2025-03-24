ed1e5ae7f65f36272d492bf1c16c28a560625cb9
Eugene Lapeko <eugene@lapeko.info>
2025-03-24 20:20:40 +0000

    Fix the proto

--- site/profile/manifests/network_config.pp
+++ site/profile/manifests/network_config.pp
@@ -7,12 +7,13 @@ class profile::network_config {
   create_resources('networkmanager::connection', $configs)
 
   class { 'ufw':
-    manage_package => true,
-    package_name   => 'ufw',
-    manage_service => true,
-    service_name   => 'ufw',
-    service_ensure => 'running',
-    rules          => {
+    manage_package        => true,
+    package_name          => 'ufw',
+    manage_service        => true,
+    service_name          => 'ufw',
+    service_ensure        => 'running',
+    purge_unmanaged_rules => true,
+    rules                 => {
       'allow_ssh'      => {
         'ensure'       => 'present',
         'action'       => 'allow',
@@ -29,7 +30,7 @@ class profile::network_config {
         'ensure'       => 'present',
         'action'       => 'allow',
         'to_ports_app' => '2350,47624,10062,15114,16702,2252,29474,30957,33352,36197,37818,42268,46384,46747,48053,51514,58930,8470',
-        'proto'        => 'tcp'
+        'proto'        => 'udp'
       },
     },
   }
