860d6027ed3e606cf104c771b88e62ea36035264
Eugene Lapeko <eugene@lapeko.info>
2025-03-24 19:46:04 +0000

    Home VPN connection

--- site/profile/manifests/network_config.pp
+++ site/profile/manifests/network_config.pp
@@ -1,5 +1,8 @@
 # This class configures the network connections on the system.
 class profile::network_config {
+  $files = lookup('network_config_files')
+  create_resources('file', $files)
+
   $configs = lookup('network_config')
   create_resources('networkmanager::connection', $configs)
 }
