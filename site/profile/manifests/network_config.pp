90619f06f99c3dd8728287e2afb865ed733c2171
Eugene Lapeko <eugene@lapeko.info>
2025-03-24 20:38:25 +0000

    Add UFW app rules

--- site/profile/manifests/network_config.pp
+++ site/profile/manifests/network_config.pp
@@ -6,6 +6,17 @@ class profile::network_config {
   $configs = lookup('network_config')
   create_resources('networkmanager::connection', $configs)
 
+  file { 'homm_ufw_app':
+    ensure  => 'file',
+    path    => '/etc/ufw/applications.d/homm',
+    content => @(EOD)
+      [HoMM]
+      title=Heroes of Might and Magic III
+      description=Heroes of Might and Magic III LAN Multiplayer
+      ports=2300,47624/tcp|2350,47624,10062,15114,16702,2252,29474,30957,33352,36197,37818,42268,46384,46747,48053,51514,58930,8470/udp
+    EOD
+  }
+
   class { 'ufw':
     manage_package        => true,
     package_name          => 'ufw',
@@ -20,17 +31,10 @@ class profile::network_config {
         'to_ports_app' => 22,
         'proto'        => 'tcp'
       },
-      'allow_homm_tcp' => {
+      'allow_homm' => {
         'ensure'       => 'present',
         'action'       => 'allow',
-        'to_ports_app' => '2300,47624',
-        'proto'        => 'tcp'
-      },
-      'allow_homm_udp' => {
-        'ensure'       => 'present',
-        'action'       => 'allow',
-        'to_ports_app' => '2350,47624,10062,15114,16702,2252,29474,30957,33352,36197,37818,42268,46384,46747,48053,51514,58930,8470',
-        'proto'        => 'udp'
+        'to_ports_app' => 'HoMM',
       },
     },
   }
