3da7a21ee2e70786b841c0df9b94e382ed99214f
Eugene Lapeko <eugene@lapeko.info>
2025-04-10 18:37:04 +0000

    use apt::source class

--- site/profile/manifests/desktop_repos.pp
+++ site/profile/manifests/desktop_repos.pp
@@ -5,37 +5,32 @@ class profile::desktop_repos {
     ensure => installed,
   }
 
-  apt { 'apt_repositories':
-    before  => Apt::Key['deb_opera_com_repo_key'],
-    sources => {
-      'deb_opera_com_repo' => {
-        location      => 'https://deb.opera.com/opera-stable/',
-        release       => 'jammy',
-        repos         => ['stable', 'non-free'],
-        architecture  => 'amd64',
-        include       => {
-          src => false,
-        },
-        key           => {
-          id     => '9701D4A1B4D92E261C8C66FE24A1004B1F11DCC9',
-          source => 'https://deb.opera.com/archive.key',
-        },
-        notify_update => true,
-      },
-      'docker_repo'        => {
-        location      => 'https://download.docker.com/linux/ubuntu',
-        release       => 'jammy',
-        repos         => ['stable'],
-        architecture  => 'amd64',
-        include       => {
-          src => false,
-        },
-        key           => {
-          id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
-          source => 'https://download.docker.com/linux/ubuntu/gpg',
-        },
-        notify_update => true,
-      },
+  apt::source { 'deb_opera_com_repo':
+    location      => 'https://deb.opera.com/opera-stable/',
+    release       => 'jammy',
+    repos         => ['stable', 'non-free'],
+    architecture  => 'amd64',
+    include       => {
+      src => false,
     },
+    key           => {
+      id     => '9701D4A1B4D92E261C8C66FE24A1004B1F11DCC9',
+      source => 'https://deb.opera.com/archive.key',
+    },
+    notify_update => true,
+  }
+  apt::source { 'docker_repo':
+    location      => 'https://download.docker.com/linux/ubuntu',
+    release       => 'jammy',
+    repos         => ['stable'],
+    architecture  => 'amd64',
+    include       => {
+      src => false,
+    },
+    key           => {
+      id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+      source => 'https://download.docker.com/linux/ubuntu/gpg',
+    },
+    notify_update => true,
   }
 }
