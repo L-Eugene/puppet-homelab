2a58120af1007546f05b5dd3f0db3c28e118dfb3
Eugene Lapeko <eugene@lapeko.info>
2025-11-27 22:43:13 +0000

    Codestyle fix

--- site/profile/manifests/vpn_private.pp
+++ site/profile/manifests/vpn_private.pp
@@ -1,19 +1,25 @@
+# @summary Manages private VPN server configuration
+#
+# This class configures an OpenVPN server for private network access,
+# including certificate files, server configuration, routing rules,
+# and firewall settings.
+#
 class profile::vpn_private {
   include openvpn::requirements
 
   $file_content = lookup('vpn_private')
 
   file { '/etc/openvpn/server/':
-    ensure  => directory,
-    mode    => "0755",
-    owner   => root,
-    group   => root
+    ensure => directory,
+    mode   => '0755',
+    owner  => root,
+    group  => root
   }
 
   file { '/etc/openvpn/server/ca.crt':
     ensure  => file,
     content => $file_content['ca'],
-    mode    => "0644",
+    mode    => '0644',
     owner   => root,
     group   => root
   }
@@ -21,7 +27,7 @@ class profile::vpn_private {
   file { '/etc/openvpn/server/dh2048.pem':
     ensure  => file,
     content => $file_content['dh'],
-    mode    => "0644",
+    mode    => '0644',
     owner   => root,
     group   => root
   }
@@ -29,7 +35,7 @@ class profile::vpn_private {
   file { '/etc/openvpn/server/vps.crt':
     ensure  => file,
     content => $file_content['cert'],
-    mode    => "0644",
+    mode    => '0644',
     owner   => root,
     group   => root
   }
@@ -37,37 +43,37 @@ class profile::vpn_private {
   file { '/etc/openvpn/server/vps.key':
     ensure  => file,
     content => $file_content['key'],
-    mode    => "0600",
+    mode    => '0600',
     owner   => root,
     group   => root
   }
 
   openvpn::server  {'server':
-    server_name => 'server',
+    server_name   => 'server',
     server_config => {
-        'port' => 1194,
-        'proto' => 'tcp',
-        'dev' => 'tun',
-        'ca' => '/etc/openvpn/server/ca.crt',
-        'cert' => '/etc/openvpn/server/vps.crt',
-        'key' => '/etc/openvpn/server/vps.key',
-        'dh' => '/etc/openvpn/server/dh2048.pem',
-        'topology' => 'subnet',
-        'server' => '10.201.0.0 255.255.255.0',
+        'port'                  => 1194,
+        'proto'                 => 'tcp',
+        'dev'                   => 'tun',
+        'ca'                    => '/etc/openvpn/server/ca.crt',
+        'cert'                  => '/etc/openvpn/server/vps.crt',
+        'key'                   => '/etc/openvpn/server/vps.key',
+        'dh'                    => '/etc/openvpn/server/dh2048.pem',
+        'topology'              => 'subnet',
+        'server'                => '10.201.0.0 255.255.255.0',
         'ifconfig-pool-persist' => '/etc/openvpn/server/ipp.txt',
-        'push' => [
+        'push'                  => [
             '"route 192.168.14.0 255.255.255.0"',
             '"route 192.168.32.0 255.255.255.0"',
             '"route 192.168.92.0 255.255.255.0"',
             '"route 192.168.116.0 255.255.255.0"',
         ],
-        'client-config-dir' => '/etc/openvpn/server/ccd',
-        'client-to-client' => '',
-        'keepalive' => '10 120',
-        'cipher' => 'AES-256-CBC',
-        'auth' => 'SHA1',
-        'persist-key' => '',
-        'persist-tun' => ''
+        'client-config-dir'     => '/etc/openvpn/server/ccd',
+        'client-to-client'      => '',
+        'keepalive'             => '10 120',
+        'cipher'                => 'AES-256-CBC',
+        'auth'                  => 'SHA1',
+        'persist-key'           => '',
+        'persist-tun'           => ''
     },
     ccd_options   => {
       'sofia'     => { 'iroute' => '192.168.116.0 255.255.255.0' },
