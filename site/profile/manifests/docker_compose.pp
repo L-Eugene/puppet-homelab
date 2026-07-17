82350591eb90ca33291ec6a493ee3f9e1ab35c51
Eugene Lapeko <eugene@lapeko.info>
2026-07-17 10:43:30 +0000

    Use upstream Docker repo for all Ubuntu versions
    The docker-ce and docker-compose-plugin packages are not available in
    official Ubuntu repos. Revert to using the upstream Docker repositories
    which have proper package availability. The apt module 10.0.1 has been
    updated to support Ubuntu 26.04 and should properly handle GPG key setup.

--- site/profile/manifests/docker_compose.pp
+++ site/profile/manifests/docker_compose.pp
@@ -1,14 +1,7 @@
 class profile::docker_compose {
-  # Ubuntu 26.04+ has docker in official repos; use those instead of upstream
-  $use_upstream_repo = if ($facts['os']['name'] == 'Ubuntu' and versioncmp($facts['os']['release']['major'], '26') >= 0) {
-    false
-  } else {
-    true
-  }
-
   class { 'docker':
     log_driver                   => 'journald',
-    use_upstream_package_source  => $use_upstream_repo,
+    use_upstream_package_source  => true,
   }
 
   include 'docker::compose'
