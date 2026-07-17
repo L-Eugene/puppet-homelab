0ed678d7aac79cd1295d1a432a4988c42b2c49f9
Eugene Lapeko <eugene@lapeko.info>
2026-07-17 10:33:06 +0000

    Use official Ubuntu repo for Docker on Ubuntu 26.04+
    - Set use_upstream_package_source to false for Ubuntu 26.04 and later
    - Falls back to upstream repos for other OS versions
    - Resolves apt_key provider compatibility issues on Ubuntu 26.04

--- site/profile/manifests/docker_compose.pp
+++ site/profile/manifests/docker_compose.pp
@@ -1,6 +1,14 @@
 class profile::docker_compose {
+  # Ubuntu 26.04+ has docker in official repos; use those instead of upstream
+  $use_upstream_repo = if ($facts['os']['name'] == 'Ubuntu' and versioncmp($facts['os']['release']['major'], '26') >= 0) {
+    false
+  } else {
+    true
+  }
+
   class { 'docker':
-    log_driver => 'journald',
+    log_driver                   => 'journald',
+    use_upstream_package_source  => $use_upstream_repo,
   }
 
   include 'docker::compose'
