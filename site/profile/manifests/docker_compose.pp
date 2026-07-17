dcc903f57e05bcbefcdb9a27f2e43cea42af4524
Eugene Lapeko <eugene@lapeko.info>
2026-07-17 10:48:38 +0000

    Use OS-specific Docker package repositories
    For Ubuntu 26.04+: Use official Ubuntu repositories with docker.io
    and docker-compose-v2 packages.
    
    For earlier Ubuntu versions: Use upstream Docker repositories with
    docker-ce and docker-compose-plugin packages.
    
    This accounts for different package naming and availability across
    Ubuntu versions.

--- site/profile/manifests/docker_compose.pp
+++ site/profile/manifests/docker_compose.pp
@@ -1,7 +1,21 @@
 class profile::docker_compose {
-  class { 'docker':
-    log_driver                   => 'journald',
-    use_upstream_package_source  => true,
+  # Ubuntu 26.04+ has docker in official repos with different package names
+  # docker.io (instead of docker-ce) and docker-compose-v2 (instead of docker-compose-plugin)
+  $use_ubuntu_repo = ($facts['os']['name'] == 'Ubuntu' and versioncmp($facts['os']['release']['major'], '26') >= 0)
+
+  if $use_ubuntu_repo {
+    # Use official Ubuntu repositories for Ubuntu 26.04+
+    class { 'docker':
+      log_driver                   => 'journald',
+      use_upstream_package_source  => false,
+      package_name                 => 'docker.io',
+    }
+  } else {
+    # Use upstream Docker repositories for earlier Ubuntu versions
+    class { 'docker':
+      log_driver                   => 'journald',
+      use_upstream_package_source  => true,
+    }
   }
 
   include 'docker::compose'
